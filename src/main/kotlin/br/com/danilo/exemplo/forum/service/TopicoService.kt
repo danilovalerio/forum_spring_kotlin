b2d4f40cb2f28b22c8e39f3f2b0b2c9d3ac337e0
danilovalerio <danilo_vs@hotmail.com>
2022-09-16 16:18:20 -0300

    transformando em service

--- src/main/kotlin/br/com/danilo/exemplo/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/service/TopicoService.kt
@@ -1,4 +1,11 @@
 package br.com.danilo.exemplo.forum.service
 
+import org.springframework.stereotype.Service
+
+/**
+ * @Service - faz com que o Spring gerencie essa classe como um serviço
+ * com isso permitindo a injeção de dependência
+ */
+@Service
 class TopicoService {
 }
