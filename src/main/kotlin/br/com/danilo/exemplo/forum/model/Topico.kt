0e8c6c5e266a9767693948bba9555ca507227770
danilovalerio <danilo_vs@hotmail.com>
2022-09-16 16:03:15 -0300

    classe de conceito resposta

--- src/main/kotlin/br/com/danilo/exemplo/forum/model/Topico.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/model/Topico.kt
@@ -12,5 +12,6 @@ data class Topico(
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
     val curso: Curso,
     val autor: Usuario,
-    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO
+    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    val respostas: List<Resposta> = ArrayList()
 )
