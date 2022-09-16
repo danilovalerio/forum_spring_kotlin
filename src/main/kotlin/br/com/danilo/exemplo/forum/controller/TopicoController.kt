759a93ec12e2e639ca308ef7d7738bb2ddd87102
danilovalerio <danilo_vs@hotmail.com>
2022-09-16 17:03:05 -0300

    metodo buscaPorId controller com os mapeamentos necessarios

--- src/main/kotlin/br/com/danilo/exemplo/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/controller/TopicoController.kt
@@ -4,7 +4,9 @@ import br.com.danilo.exemplo.forum.model.Curso
 import br.com.danilo.exemplo.forum.model.Topico
 import br.com.danilo.exemplo.forum.model.Usuario
 import br.com.danilo.exemplo.forum.service.TopicoService
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Retry.Topic
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -24,6 +26,15 @@ class TopicoController(private val service: TopicoService) {
          * service me devolve o listar
          */
         return service.listar()
+    }
+
+    /**
+     * adicionado o complemento /{id} da uri
+     * @PathVariable indica que id é uma variavel do caminho
+     */
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico {
+        return service.buscaPorId(id)
     }
 
 }
