38c908ffdbb6c9a0685ea228dfa91d7d2d1f1883
danilovalerio <danilo_vs@hotmail.com>
2022-09-19 17:01:21 -0300

    Criacao da nossa TopicoViewMapper para ficar com a responsabilidade de mapeamento dos dados

---
+++ src/main/kotlin/br/com/danilo/exemplo/forum/mapper/TopicoViewMapper.kt
@@ -0,0 +1,21 @@
+package br.com.danilo.exemplo.forum.mapper
+
+import br.com.danilo.exemplo.forum.dto.TopicoView
+import br.com.danilo.exemplo.forum.model.Topico
+import org.springframework.stereotype.Component
+
+/**
+ * Converte de um Topico (model) para um TopicoView
+ * Sera um @Component (componente) por questão de semantica, mas poderia ser service
+ */
+@Component
+class TopicoViewMapper: Mapper<Topico, TopicoView> {
+    override fun map(topico: Topico): TopicoView {
+        return TopicoView(
+            id = topico.id,
+            titulo = topico.titulo,
+            mensagem = topico.mensagem,
+            dataCriacao = topico.dataCriacao,
+            status = topico.status)
+    }
+}
