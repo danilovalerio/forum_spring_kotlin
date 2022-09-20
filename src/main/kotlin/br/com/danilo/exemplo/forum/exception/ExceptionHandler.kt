eeb14b79119928086894e8d7b0f2c53d0e4d1dae
danilovalerio <danilo_vs@hotmail.com>
2022-09-20 17:59:24 -0300

    Classe ExceptionHandler para tratar as exceptions lançadas

---
+++ src/main/kotlin/br/com/danilo/exemplo/forum/exception/ExceptionHandler.kt
@@ -0,0 +1,25 @@
+package br.com.danilo.exemplo.forum.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+/**
+ * Classe para fazer o Tratamento das Exceptions Lançadas
+ *
+ * @RestControllerAdvice - Para parametrizar para o Spring que essa classe faz um tratamento para os RestsControllers
+ * da nossa aplicação (Advice)
+ */
+@RestControllerAdvice
+class ExceptionHandler {
+    /**
+     * @exceptionHandler e especifica quando for NotFoundException chama essa função
+     * e devolve o codigo NOT_FOUND
+     */
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFound(): ErroView{
+
+    }
+}
