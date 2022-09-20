10b0c259a02f83e0eb9255b54548b3d743ff654f
danilovalerio <danilo_vs@hotmail.com>
2022-09-20 18:59:51 -0300

    Adicionando MENSAGENS para substituir as padroes do Spring Boot

--- src/main/kotlin/br/com/danilo/exemplo/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/dto/NovoTopicoForm.kt
@@ -5,13 +5,13 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm (
-    @field:NotEmpty
-    @field:Size(min = 5, max = 30)
+    @field:NotEmpty(message = "Título não pode ser em branco")
+    @field:Size(min = 5, max = 30, message = "Título deve ter entre 5 e 30 caracteres")
     val titulo: String,
-    @field:NotEmpty
+    @field:NotEmpty(message = "Mensagem não pode ser em branco")
     val mensagem: String,
-    @field:NotNull
+    @field:NotNull(message = "Id do curso não pode ser nulo")
     val idCurso: Long,
-    @field:NotNull
+    @field:NotNull(message = "Id do autor não pode ser nulo")
     val idAutor: Long
 )
