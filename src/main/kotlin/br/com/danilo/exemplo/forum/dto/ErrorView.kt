85c9d72d302dc91c576e8f0cacfe9084b959c7c0
danilovalerio <danilo_vs@hotmail.com>
2022-09-20 18:06:17 -0300

    DTO ErrorView generico para nossas exceptions

--- src/main/kotlin/br/com/danilo/exemplo/forum/dto/ErrorView.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/dto/ErrorView.kt
@@ -9,6 +9,6 @@ data class ErrorView (
     val timestamp: LocalDateTime = LocalDateTime.now(),
     val status: Int,
     val error: String,
-    val message: String,
+    val message: String?,
     val path: String
 )
--- src/main/kotlin/br/com/danilo/exemplo/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/exception/ExceptionHandler.kt
@@ -5,22 +5,33 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.servlet.http.HttpServletRequest
 
 /**
  * Classe para fazer o Tratamento das Exceptions Lançadas
  *
  * @RestControllerAdvice - Para parametrizar para o Spring que essa classe faz um tratamento para os RestsControllers
- * da nossa aplicação (Advice)
+ * da nossa aplicação (Advice), o Spring vai olhar aqui quando der alguma exception e ver se tem um tratamento personalizado
  */
 @RestControllerAdvice
 class ExceptionHandler {
     /**
      * @exceptionHandler e especifica quando for NotFoundException chama essa função
      * e devolve o codigo NOT_FOUND
+     *
+     * HttpServeletRequest - Permite pegar o caminho da requisição
      */
     @ExceptionHandler(NotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleNotFound(): ErrorView {
-
+    fun handleNotFound(
+        exception: NotFoundException,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.NOT_FOUND.value(),
+            error = HttpStatus.NOT_FOUND.name,
+            message = exception.message,
+            path = request.servletPath
+        )
     }
 }
