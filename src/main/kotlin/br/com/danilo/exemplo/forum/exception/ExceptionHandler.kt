27b32481ca57188c37e93f5bd7510c99716cc49b
danilovalerio <danilo_vs@hotmail.com>
2022-09-20 18:51:40 -0300

    MethodArgumentNotValidException - Tratado com handleValidationError

--- src/main/kotlin/br/com/danilo/exemplo/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.danilo.exemplo.forum.exception
 
 import br.com.danilo.exemplo.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -35,9 +36,26 @@ class ExceptionHandler {
         )
     }
 
+    /**
+     * BAD_REQUEST - Pois foi uma requisição invalida enviada pelo cliente com campo obrigatório em branco por exemplo
+     */
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun handleNotFound(
+    fun handleServerError(
         exception: Exception,
         request: HttpServletRequest
     ): ErrorView {
