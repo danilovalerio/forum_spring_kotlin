603a685b1f6157219269a610ebec07464e843683
danilovalerio <danilo_vs@hotmail.com>
2022-09-21 11:13:21 -0300

    Mapeamento para o JPA

--- src/main/kotlin/br/com/danilo/exemplo/forum/model/Topico.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/model/Topico.kt
@@ -1,17 +1,25 @@
 package br.com.danilo.exemplo.forum.model
 
 import java.time.LocalDateTime
+import javax.persistence.*
 
 /**
  * Representa o recruso de tópico dentro do projeto
  */
+
+@Entity
 data class Topico(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
     val titulo: String,
     val mensagem: String,
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val curso: Curso,
+    @ManyToOne
     val autor: Usuario,
+    @Enumerated(value = EnumType.STRING)
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    @OneToMany
     val respostas: List<Resposta> = ArrayList()
 )
