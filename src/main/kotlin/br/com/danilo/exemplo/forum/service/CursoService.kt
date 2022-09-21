993f0818ceb45e85c082304d63e0656803e92550
danilovalerio <danilo_vs@hotmail.com>
2022-09-21 12:04:43 -0300

    Remocao do banco simulado com listas e implementacao do Repository para uso no banco de dados

--- src/main/kotlin/br/com/danilo/exemplo/forum/service/CursoService.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/service/CursoService.kt
@@ -1,23 +1,15 @@
 package br.com.danilo.exemplo.forum.service
 
 import br.com.danilo.exemplo.forum.model.Curso
+import br.com.danilo.exemplo.forum.repository.CursoRepository
 import org.springframework.stereotype.Service
 import java.util.Arrays
 
 @Service
 class CursoService (
-    var cursos: List<Curso>
+    private val repository: CursoRepository
 ) {
-    init {
-        val curso = Curso(id = 1, nome = "Kotlin", categoria = "Programacao")
-        val curso2 = Curso(id = 2, nome = "Spring Boot", categoria = "Programacao")
-
-        cursos = Arrays.asList(curso, curso2)
-    }
-
     fun buscaPorId(id: Long): Curso {
-        return cursos.stream().filter({
-            curso -> curso.id == id
-        }).findFirst().get()
+        return repository.getReferenceById(id)
     }
 }
--- src/main/kotlin/br/com/danilo/exemplo/forum/service/UsuarioService.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/service/UsuarioService.kt
@@ -1,22 +1,17 @@
 package br.com.danilo.exemplo.forum.service
 
 import br.com.danilo.exemplo.forum.model.Usuario
+import br.com.danilo.exemplo.forum.repository.UsuarioRepository
 import org.springframework.stereotype.Service
 import java.util.Arrays
 
 @Service
-data class UsuarioService(var usuarios: List<Usuario>) {
-    init {
-        val user1 = Usuario(id = 1, nome = "Danilo", email = "danilo_vs@hotmail.com")
-        val user2 = Usuario(id = 2, nome = "Daniela", email = "danivviana@gmail.com")
-
-        usuarios = Arrays.asList(user1, user2)
-    }
+data class UsuarioService(
+    private val repository: UsuarioRepository
+) {
 
     fun buscaPorId(id: Long): Usuario {
-        return usuarios.stream().filter({
-            user -> user.id == id
-        }).findFirst().get()
+        return repository.getOne(id)
     }
 
 }
