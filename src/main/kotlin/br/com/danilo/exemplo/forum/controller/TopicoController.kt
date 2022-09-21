afc83f067fb087514bfac4f5da7cbbc55bea8510
danilovalerio <danilo_vs@hotmail.com>
2022-09-21 18:33:12 -0300

    Ativando o cache para a lista de topicos

--- src/main/kotlin/br/com/danilo/exemplo/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/controller/TopicoController.kt
@@ -4,6 +4,7 @@ import br.com.danilo.exemplo.forum.dto.AtualizacaoTopicoForm
 import br.com.danilo.exemplo.forum.dto.NovoTopicoForm
 import br.com.danilo.exemplo.forum.dto.TopicoView
 import br.com.danilo.exemplo.forum.service.TopicoService
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -26,6 +27,7 @@ import javax.validation.Valid
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
+    @Cacheable("topicos")
     fun listar(
         @RequestParam(required = false) nomeCurso: String?,
         @PageableDefault(size = 5, sort = ["dataCriacao"], direction = Sort.Direction.DESC)
