58ae950f14ff1cee7f2cfbcd17d98620183cf56f
danilovalerio <danilo_vs@hotmail.com>
2022-09-26 16:39:47 -0300

    Criando as Roles para o Usuario

--- src/main/kotlin/br/com/danilo/exemplo/forum/model/Usuario.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/model/Usuario.kt
@@ -1,16 +1,21 @@
 package br.com.danilo.exemplo.forum.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import com.fasterxml.jackson.annotation.JsonIgnore
+import javax.persistence.*
 
+/**
+ * @JsonIgnore - para não exibir as roles do usuario quando consultado o recurso
+ * @ManyToMany com fetchType EAGER para quando carregar o usuário busque também todas as roles imediatamente
+ */
 @Entity
 data class Usuario (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val nome: String,
     val email: String,
-    val password: String
-
+    val password: String,
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "usuario_role")
+    val role: List<Role> = mutableListOf()
 )
--- src/main/kotlin/br/com/danilo/exemplo/forum/service/UserDetail.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/service/UserDetail.kt
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails
  * Classe para ter os detalhes do usuario
  */
 class UserDetail(private val usuario: Usuario) : UserDetails {
-    override fun getAuthorities() = null
+    override fun getAuthorities() = usuario.role
 
     override fun getPassword() = usuario.password
 
