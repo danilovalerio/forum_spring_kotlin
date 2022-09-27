7e05b51b9405046ed4a12d843ffee0edf8dc3594
danilovalerio <danilo_vs@hotmail.com>
2022-09-27 08:35:15 -0300

    Config do Login Filter

--- src/main/kotlin/br/com/danilo/exemplo/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/danilo/exemplo/forum/config/SecurityConfiguration.kt
@@ -1,5 +1,6 @@
 package br.com.danilo.exemplo.forum.config
 
+import br.com.danilo.exemplo.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
---
+++ src/main/kotlin/br/com/danilo/exemplo/forum/security/JWTLoginFilter.kt
@@ -0,0 +1,41 @@
+package br.com.danilo.exemplo.forum.security
+
+import br.com.danilo.exemplo.forum.config.JWUtil
+import br.com.danilo.exemplo.forum.model.Credentials
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JWTLoginFilter(
+    private val authManager: AuthenticationManager,
+    private val jwtUtil: JWUtil)
+    : UsernamePasswordAuthenticationFilter() {
+
+    override fun attemptAuthentication(request: HttpServletRequest?, response: HttpServletResponse?): Authentication {
+        // jason mapeia o objeto no caso o da requisica e passa para o nosso tipo definido
+        val (username, password) = ObjectMapper().readValue(request?.inputStream, Credentials::class.java)
+        val token = UsernamePasswordAuthenticationToken(username, password)
+        return authManager.authenticate(token)
+    }
+
+    // ao dar sucesso na autenticação
+    override fun successfulAuthentication(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        chain: FilterChain?,
+        authResult: Authentication?
+    ) {
+        val username = (authResult?.principal as UserDetails).username
+        val token = jwtUtil.generateToken(username)
+        // adiciona no header da resposta
+        response?.addHeader("Authorization", "Bearer $token")
+    }
+
+
+}
