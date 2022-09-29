8a34bf620d79b7e94d30280ccb3ad59f8f05097c
danilovalerio <danilo_vs@hotmail.com>
2022-09-29 18:40:13 -0300

    inserir um topico e adicionar o campo data alteracao na tabela topico

---
+++ src/main/resources/db.migration/V9__insert_topico.sql
@@ -0,0 +1,2 @@
+insert into topico(id, titulo, mensagem, data_criacao, status, curso_id, autor_id)
+    values(1, 'duvida sobre kotlin', 'minha funcao let não funfa', '2021-12-24 12:00:00','NAO_RESPONDIDO',1,1)
