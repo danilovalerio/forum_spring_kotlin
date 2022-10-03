8fe2e29c70f567b945929b1349bb957936fc9e0c
danilovalerio <danilo_vs@hotmail.com>
2022-10-03 10:25:08 -0300

    Teste para verficar a chamada

--- src/test/kotlin/br/com/danilo/exemplo/forum/service/TopicoServiceTest.kt
+++ src/test/kotlin/br/com/danilo/exemplo/forum/service/TopicoServiceTest.kt
@@ -3,10 +3,14 @@ package br.com.danilo.exemplo.forum.service
 import br.com.danilo.exemplo.forum.mapper.TopicoFormMapper
 import br.com.danilo.exemplo.forum.mapper.TopicoViewMapper
 import br.com.danilo.exemplo.forum.model.TopicoTest
+import br.com.danilo.exemplo.forum.model.TopicoViewTest
 import br.com.danilo.exemplo.forum.repository.TopicoRepository
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
 import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import javax.persistence.EntityManager
 
 class TopicoServiceTest {
@@ -16,6 +20,8 @@ class TopicoServiceTest {
      * se não chama o finAll
      */
     val topicos = PageImpl(listOf(TopicoTest.build()))
+
+    val paginacao: Pageable = mockk()
 
     val topicoRepository: TopicoRepository = mockk {
         //Sempre que topicoRepository for chamado com qualqquer parametro
@@ -28,4 +34,17 @@ class TopicoServiceTest {
     val topicoService = TopicoService(
         topicoRepository, topicoViewMapper, topicoFormMapper, "", em
     )
+
+    @Test
+    fun `deve listar topicos a partir do nome do curso`(){
+        // sempre que topicoViewMapper.map for chamado com qualquer parametro retorna nosso objeto mok=ckado
+        every { topicoViewMapper.map(any()) } returns TopicoViewTest.build()
+
+        topicoService.listar("Kotlin Básico", paginacao = paginacao)
+
+        verify(exactly = 1) { topicoRepository.findByCursoNome(any(), any()) }
+        verify(exactly = 1) { topicoViewMapper.map(any()) }
+        verify(exactly = 0) { topicoRepository.findAll(paginacao) }
+
+    }
 }
