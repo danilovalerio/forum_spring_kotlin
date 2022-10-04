bf8d03afd49e1b6bc71cc5fea574073c2daa7572
danilovalerio <danilo_vs@hotmail.com>
2022-10-04 12:14:43 -0300

    Classe com funcao para envio de email

---
+++ src/main/kotlin/br/com/danilo/exemplo/forum/service/EmailService.kt
@@ -0,0 +1,22 @@
+package br.com.danilo.exemplo.forum.service
+
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+@Service
+class EmailService(
+    private val javaMailSender: JavaMailSender
+) {
+
+    fun notificar(){
+        val message = SimpleMailMessage()
+
+        message.setSubject("")
+        message.setText("")
+        message.setTo("")
+
+        javaMailSender.send(message)
+    }
+
+}
